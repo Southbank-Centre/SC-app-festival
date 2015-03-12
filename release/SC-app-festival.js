34d0d24232f918eeb471aa9197bbe7d4ee094e3a
samueljmurray <samueljmurray@gmail.com>
2015-03-12 11:57:37 +0000

    initial commit

---
+++ release/SC-app-festival.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name SC-app-festival
+ * @description
+ *
+ * Provides the app with the ability to display event content and features
+ */
+angular
+  .module('SC-app-festival');
