f99a99ed35e194f73e729b70ab0b2c023a0a2180
samueljmurray <samueljmurray@gmail.com>
2015-03-12 12:03:47 +0000

    corrected documentation and removed unnessary dependencies and files

--- app/SC-app-festival.module.js
+++ app/SC-app-festival.module.js
@@ -5,7 +5,7 @@
  * @name SC-app-festival
  * @description
  *
- * Provides the app with the ability to display event content and features
+ * Provides the app with the ability to display festival content and features
  */
 angular
   .module('SC-app-festival');
